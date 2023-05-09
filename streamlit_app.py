1b8ae687593bd8468e42edc8d4d34d7ee19fc14c
YenSunHwang <80447470+YenSunHwang@users.noreply.github.com>
2023-05-09 18:53:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,41 +48,34 @@ except URLEroor as e:
    streamlit.error()
 
 
-
-
-#don't run anything past here while we troubleshoot
-
-
-
 streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
+#Snowflake ...
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from pc_rivery_db.public.fuit_load_list")
-         return my_cur.fetchall()
-    
-#add a button to load the fruit
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button ...
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-
-
     
-# allow the end user to add a fruit to the list
+# Allow the ...
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
-         return "Thanks for adding" + new_fruit
-          
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
-    my_cnx=snowflake.connector.connect(**streamlit.secrets["Snowflake"])
-    back_from_function=insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
-
-streamlit.stop()
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.write(back_from_function)    
 
 
-# This will not work correctly, but just go with it for now
+
+
+
+
 
